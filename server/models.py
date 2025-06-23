af06dbcad24968f148e75336da7d192a099a5d1a
Salman <salmaanmohamed700@gmail.com>
2025-06-23 12:59:03 +0300

    removed an error

--- server/models.py
+++ server/models.py
@@ -1,11 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
-class Plant(db.Model):
+class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     image = db.Column(db.String)
     price = db.Column(db.Float)
+
+    serialize_rules = ('-some_relationship_if_any',)  # optional; remove if not using
